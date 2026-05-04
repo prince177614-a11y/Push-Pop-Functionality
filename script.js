bfbd4f8e55bc82a7cb0f8c0b9df6d1d56ac298e6
Prince Gupta <prince177614@gmail.com>
2026-05-04 22:08:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//1. push => Koi bhi new data ko insert karega from last.
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// let num;
+// do{
+//     num = Number(prompt("Enter a number"));
+//     arr.push(num);
+// }
+// while(num <= 100)
+
+// console.log(arr);
+
+
+//2. pop => data remove karta hai from last.
+
+// let arr1 = [11, 22, 33, 55, 44, 88];
+
+// arr1.pop()
+// console.log(arr1)
+// arr1.pop()
+// console.log(arr1)
+
+
+//3. shift => data remove karta hai from start.
+
+// let names = ["prince", "pranshu", "praanu", "pagal"];
+
+// names.shift();
+// console.log(names);
+
+
+//4. unshift => koi bhi new data ko insert karega from start.
+
+// let names = ["prince", "pranshu", "praanu", "pagal"];
+
+// names.unshift("pranshu", "prince", "praanu", "pagal");
+// console.log(names);
+
+
+//5. length => array ki length batata hai.
+
+// let names = ["prince", "pranshu", "praanu", "pagal"];
+
+// console.log(names.length);
